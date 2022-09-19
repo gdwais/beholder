397217aa7df234279f165e3af7b2eea35da69d4c
Dalton Wais <dalton.wais@getbased.com>
2022-09-19 08:44:05 -0600

    adding trait dropdown

--- beholder-ui/src/App.tsx
+++ beholder-ui/src/App.tsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import { ImageCard } from "./components";
-import { Grid, ImageList } from "@mui/material";
+import {
+  Grid,
+  ImageList,
+  Select,
+  InputLabel,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
 
 import { Nft } from "./types";
 import apiClient from "./client";
@@ -10,21 +17,38 @@ import apiClient from "./client";
 function App() {
   const [nfts, setNfts] = useState<Nft[]>([]);
   const [partialList, setPartialList] = useState<Nft[]>([]);
+  const [traits, setTraits] = useState<string[]>([]);
+  const [selectedTrait, setSelectedTrait] = useState<string | undefined>(
+    undefined
+  );
+
+  const loadNfts = async (selectedTrait: string) => {
+    const response = await apiClient.get(`/predictions/${selectedTrait}`);
+    if (response && response.data) {
+      setNfts(response.data);
+      setPartialList([...response.data.slice(0, 10)]);
+    }
+  };
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    const target = event.target;
+    console.log(`handleChange `, target);
+    setSelectedTrait(target.value);
+    loadNfts(target.value);
+  };
 
   useEffect(() => {
-    const loadNfts = async () => {
-      const response = await apiClient.get("/predictions");
+    const loadTraits = async () => {
+      const response = await apiClient.get("/traits");
       if (response && response.data) {
-        setNfts(response.data);
-        setPartialList([...response.data.slice(0, 10)]);
+        setTraits(response.data);
       }
     };
 
-    loadNfts();
+    loadTraits();
   }, []);
 
   const queryMore = () => {
-    console.log(`queryMore`);
     const updatedDataList = [
       ...partialList,
       ...nfts.slice(partialList.length, partialList.length + 10),
@@ -38,19 +62,31 @@ function App() {
         <h1>Beholder!</h1>
       </header>
       <body className="app-body">
-      <Grid container spacing={4}>
-        {partialList.map((nft) => (
-          <Grid item xs={2}>
-          <ImageCard
-            mint={nft.mint}
-            image={nft.image}
-            name={nft.name}
-            traits={nft.traits}
-            queryMore={queryMore}
-          />
-          </Grid>
-        ))}
-      </Grid>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={selectedTrait}
+          label="Age"
+          onChange={handleChange}
+        >
+          {traits.map((t) => (
+            <MenuItem value={t}>{t}</MenuItem>
+          ))}
+        </Select>
+        <Grid container spacing={4}>
+          {partialList.map((nft) => (
+            <Grid item xs={2}>
+              <ImageCard
+                mint={nft.mint}
+                image={nft.image}
+                name={nft.name}
+                traits={nft.traits}
+                queryMore={queryMore}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </body>
     </div>
   );
