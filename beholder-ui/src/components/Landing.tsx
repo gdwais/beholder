00efcb6044d81b350c48c03c58dd80d75f789ba9
Dalton Wais <dalton.wais@getbased.com>
2022-12-19 08:38:40 -0700

    rework landing page

--- beholder-ui/src/components/Landing.tsx
+++ beholder-ui/src/components/Landing.tsx
@@ -1,36 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 
-
-
-const LandingFrame = () => {
-
-    return <div className="bg-gradient-to-l md:bg-gradient-to-r h-[100%] w-[100%]"><LandingFrameMessage /></div>
+type HeaderProps = {
+    brand: string;
+    activeClass: boolean;
+    setActiveClass: (value: boolean) => void;
 }
 
-const LandingFrameMessage = () => {
-    const style = {
-        margin: "auto",
-        padding: "10% 35% 10% 15%",
-        color: "white"
-    }
-    return <div style={style}>
-        
-        <div className="font-[96px]">
-            Hello World!!
-        </div>
-        
-        <div className="font-[36px]">
-            This is the landing page and here's some content.
-            How much wood would a woodchuck chuck 
-            if a woodchuck would chuck wood?
-        </div>
-        <br />
-    </div>
-}
 
-export const Landing = () => {
-    return <div>
-        <LandingFrame />
-    </div>
-    
-}
+const Header = (props: HeaderProps) => {
+    return (
+      <header>
+        <h2 className="logo">{props.brand}</h2>
+        <div
+          onClick={() => {
+            props.setActiveClass(!props.activeClass);
+          }}
+          className={props.activeClass ? "toggle active" : "toggle"}
+        ></div>
+      </header>
+    );
+  };
+
+  type ContentsProps = {
+    title: string;
+    subtitle: string;
+    content: string;
+  }
+  
+  const Contents = (props: ContentsProps) => {
+    return (
+      <div className="text">
+        <h2> {props.title} </h2>
+        <h3> {props.subtitle} </h3>
+        <p> {props.content} </p>
+        <a href="#">Browse</a>
+      </div>
+    );
+  };
+  
+  const Background = () => {
+    return <div className="from-purple-600 to-blue-600 bg-gradient-to-l md:bg-gradient-to-r"></div>
+  }
+
+  const Overlay = () => {
+    return <div className="overlay"> </div>;
+  };
+  
+  type FooterProps = {
+    content: string;
+  }
+
+  const Footer = (props: FooterProps) => {
+    return (
+      <div className="footer">
+        {" "}
+        <p>{props.content} </p>{" "}
+      </div>
+    );
+  };
+  const Menu = () => {
+    return (
+      <div className="menu">
+        <ul>
+          <li>
+            <a href="#"> Home </a>
+          </li>
+          <li>
+            <a href="#"> Home </a>
+          </li>
+          <li>
+            <a href="#"> Home </a>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+  export const Landing = () => {
+    const [activeClass, setActiveClass] = useState<boolean>(false);
+  
+    return (
+      <div>
+        <section className={activeClass ? "active container" : "container"}>
+          <Header
+            setActiveClass={setActiveClass}
+            activeClass={activeClass}
+            brand="this brand"
+          />
+          <Background />
+          <Overlay />
+          <Contents
+            title="BEHOLDER"
+            subtitle="We see all."
+            content="ML powered trait regognition for nfts"
+          />
+          <Footer content="Copyright 2022" />
+        </section>
+  
+        <Menu />
+      </div>
+    );
+  };
+  
+  
